0abaa8466dc2153664c5c6a8437a095ead23f874
FR1234 <florisrietman@gmail.com>
2024-04-23 19:40:08 +0200

    Meer vertaalt

--- to-do/db.js
+++ to-do/db.js
@@ -1,6 +1,6 @@
-const DB_FILE_NAME = 'pwa-to-do/db-v5';
-const DEFAULT_LIST_NAME = 'Things to do'
-const DEFAULT_LIST_COLOR = 'peachpuff';
+const DB_FILE_NAME = "pwa-to-do/db-v5";
+const DEFAULT_LIST_NAME = "Huiswerk";
+const DEFAULT_LIST_COLOR = "peachpuff";
 const SQL_NEW_TABLES = `
   CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, title TEXT NOT NULL, completed INTEGER NOT NULL DEFAULT 0, notes TEXT, has_file INTEGER NOT NULL DEFAULT 0, due_by INTEGER, list_id INTEGER);
   CREATE TABLE IF NOT EXISTS lists (id INTEGER PRIMARY KEY, title TEXT, color TEXT);
@@ -14,7 +14,10 @@ function loadSQLite() {
   if (!sqliteLoader) {
     sqliteLoader = new Promise((resolve, reject) => {
       let script = document.createElement("script");
-      script.setAttribute("src", "./sqlite-wasm-3400000/jswasm/sqlite3-worker1-promiser.js");
+      script.setAttribute(
+        "src",
+        "./sqlite-wasm-3400000/jswasm/sqlite3-worker1-promiser.js"
+      );
       script.setAttribute("async", "true");
       document.body.appendChild(script);
       script.addEventListener("load", resolve);
@@ -91,43 +94,45 @@ export async function getDBFile() {
 
 export async function createTask(title, listId) {
   await sqlitePromiser("exec", {
-    sql: `INSERT INTO tasks (title, completed, notes, has_file, due_by, list_id) VALUES ('${title}', 0, '', 0, 0, ${listId});`
+    sql: `INSERT INTO tasks (title, completed, notes, has_file, due_by, list_id) VALUES ('${title}', 0, '', 0, 0, ${listId});`,
   });
 }
 
 export async function addTaskNotes(taskId, notes) {
   await sqlitePromiser("exec", {
-    sql: `UPDATE tasks SET notes = '${notes}' WHERE id = ${taskId};`
+    sql: `UPDATE tasks SET notes = '${notes}' WHERE id = ${taskId};`,
   });
 }
 
 export async function renameTask(taskId, title) {
   await sqlitePromiser("exec", {
-    sql: `UPDATE tasks SET title = '${title}' WHERE id = ${taskId};`
+    sql: `UPDATE tasks SET title = '${title}' WHERE id = ${taskId};`,
   });
 }
 
 export async function completeTask(taskId, completed) {
   await sqlitePromiser("exec", {
-    sql: `UPDATE tasks SET completed = ${completed ? 1 : 0} WHERE id = ${taskId};`
+    sql: `UPDATE tasks SET completed = ${
+      completed ? 1 : 0
+    } WHERE id = ${taskId};`,
   });
 }
 
 export async function setTaskHasFile(taskId, hasFile) {
   await sqlitePromiser("exec", {
-    sql: `UPDATE tasks SET has_file = ${hasFile ? 1 : 0} WHERE id = ${taskId};`
+    sql: `UPDATE tasks SET has_file = ${hasFile ? 1 : 0} WHERE id = ${taskId};`,
   });
 }
 
 export async function setTaskDueBy(taskId, dueBy) {
   await sqlitePromiser("exec", {
-    sql: `UPDATE tasks SET due_by = ${dueBy} WHERE id = ${taskId};`
+    sql: `UPDATE tasks SET due_by = ${dueBy} WHERE id = ${taskId};`,
   });
 }
 
 export async function deleteTask(taskId) {
   await sqlitePromiser("exec", {
-    sql: `DELETE FROM tasks WHERE id = ${taskId};`
+    sql: `DELETE FROM tasks WHERE id = ${taskId};`,
   });
 }
 
@@ -135,7 +140,8 @@ export async function createList(title, color) {
   const list = await sqlitePromiser("exec", {
     sql: `INSERT INTO lists (title, color) VALUES ('${title}', '${color}');
           SELECT * FROM lists WHERE rowid = (SELECT last_insert_rowid());`,
-    resultRows: [], columnNames: [],
+    resultRows: [],
+    columnNames: [],
   });
 
   return formatResult(list);
@@ -143,13 +149,13 @@ export async function createList(title, color) {
 
 export async function renameList(listId, title) {
   await sqlitePromiser("exec", {
-    sql: `UPDATE lists SET title = '${title}' WHERE id = ${listId};`
+    sql: `UPDATE lists SET title = '${title}' WHERE id = ${listId};`,
   });
 }
 
 export async function changeListColor(listId, color) {
   await sqlitePromiser("exec", {
-    sql: `UPDATE lists SET color = '${color}' WHERE id = ${listId};`
+    sql: `UPDATE lists SET color = '${color}' WHERE id = ${listId};`,
   });
 }
 
@@ -161,7 +167,8 @@ export async function getLists() {
       LEFT JOIN tasks ON lists.id = tasks.list_id
       GROUP BY lists.id, lists.title, lists.color;
     `,
-    resultRows: [], columnNames: [],
+    resultRows: [],
+    columnNames: [],
   });
 
   return formatResult(lists);
@@ -170,7 +177,8 @@ export async function getLists() {
 export async function getListTasks(listId) {
   const tasks = await sqlitePromiser("exec", {
     sql: `SELECT * FROM tasks WHERE list_id = ${listId};`,
-    resultRows: [], columnNames: [],
+    resultRows: [],
+    columnNames: [],
   });
 
   return formatResult(tasks);
@@ -183,7 +191,8 @@ export async function searchTasks(query) {
       LEFT JOIN lists ON tasks.list_id = lists.id
       WHERE (tasks.title LIKE '%${query}%' OR tasks.notes LIKE '%${query}%');
     `,
-    resultRows: [], columnNames: [],
+    resultRows: [],
+    columnNames: [],
   });
 
   return formatResult(tasks);
@@ -202,9 +211,12 @@ export async function getTodaysTasks() {
     sql: `
       SELECT tasks.*, lists.title as list_title, lists.color as list_color FROM tasks
       LEFT JOIN lists ON tasks.list_id = lists.id
-      WHERE tasks.completed = 0 AND tasks.due_by > 0 AND tasks.due_by >= ${start} AND tasks.due_by < ${start + 86400000};
+      WHERE tasks.completed = 0 AND tasks.due_by > 0 AND tasks.due_by >= ${start} AND tasks.due_by < ${
+      start + 86400000
+    };
     `,
-    resultRows: [], columnNames: [],
+    resultRows: [],
+    columnNames: [],
   });
 
   return formatResult(tasks);
@@ -225,7 +237,8 @@ export async function getOverdueTasks() {
       LEFT JOIN lists ON tasks.list_id = lists.id
       WHERE tasks.completed = 0 AND tasks.due_by > 0 AND tasks.due_by < ${time};
     `,
-    resultRows: [], columnNames: [],
+    resultRows: [],
+    columnNames: [],
   });
 
   return formatResult(tasks);
@@ -233,10 +246,10 @@ export async function getOverdueTasks() {
 
 export async function deleteList(listId) {
   await sqlitePromiser("exec", {
-    sql: `DELETE FROM lists WHERE id = ${listId};`
+    sql: `DELETE FROM lists WHERE id = ${listId};`,
   });
   await sqlitePromiser("exec", {
-    sql: `DELETE FROM tasks WHERE list_id = ${listId};`
+    sql: `DELETE FROM tasks WHERE list_id = ${listId};`,
   });
 }
 
@@ -257,11 +270,12 @@ function formatResult(selectResult) {
 window.exec = async function (sql) {
   const res = await sqlitePromiser("exec", {
     sql,
-    resultRows: [], columnNames: [],
+    resultRows: [],
+    columnNames: [],
   });
 
   console.log(formatResult(res));
-}
+};
 
 // TODO: REMOVE, ONLY FOR DEBUG
 window.getTodaysTasks = getTodaysTasks;
