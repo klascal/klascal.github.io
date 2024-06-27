c7174d469c04d1a187b1b4be3c4050eb0cbbc35a
BAAS-VVG <baas@csvvg.eu>
2024-06-27 13:58:07 +0200

    Access token wordt nu ook in de local storage opgeslagen.

--- scheduleDay.js
+++ scheduleDay.js
@@ -48,7 +48,7 @@ async function fetchToken(authorizationCode, schoolName) {
 }
 
 // Function to fetch appointments for the specified date
-function fetchAppointments(date) {
+async function fetchAppointments(date) {
   // Parse the input date string to get the date and month
   const [day, monthName] = date.split(" ");
   const monthIndex = dutchMonthNames.findIndex(
@@ -76,140 +76,145 @@ function fetchAppointments(date) {
   const startTimestamp = Math.floor(startDate.getTime() / 1000);
   const endTimestamp = Math.floor(endDate.getTime() / 1000);
 
-  fetchToken(authorizationCode, schoolName).then((accessToken) => {
-    const apiUrl = `https://${schoolName}.zportal.nl/api/v3/appointments?user=${user}&start=${startTimestamp}&end=${endTimestamp}&valid=true&fields=subjects,cancelled,locations,startTimeSlot,start,end,groups,teachers,changeDescription&access_token=${accessToken}`;
+    // Wissel de koppelcode in voor de access token (maar alleen als die nog niet in local storage staat)
+  let accessToken = localStorage.getItem("access_token");
+  if (accessToken == null || accessToken == "undefined") {
+    accessToken = await fetchToken(authorizationCode, schoolName);
+    localStorage.setItem("access_token", accessToken);
+  }
 
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        const appointments = data.response.data;
-        // Sort appointments by start time
-        appointments.sort((a, b) => a.start - b.start);
-        const scheduleDiv = document.getElementById("schedule");
-        scheduleDiv.innerHTML = ""; // Clear existing schedule
-        const errorMessageDiv = document.getElementById("error-message");
+  const apiUrl = `https://${schoolName}.zportal.nl/api/v3/appointments?user=${user}&start=${startTimestamp}&end=${endTimestamp}&valid=true&fields=subjects,cancelled,locations,startTimeSlot,start,end,groups,teachers,changeDescription&access_token=${accessToken}`;
 
-        if (appointments.length === 0) {
-          scheduleDiv.style.display = "none";
-          errorMessageDiv.style.display = "block";
-        } else {
-          scheduleDiv.style.display = "block";
-          errorMessageDiv.style.display = "none";
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const appointments = data.response.data;
+      // Sort appointments by start time
+      appointments.sort((a, b) => a.start - b.start);
+      const scheduleDiv = document.getElementById("schedule");
+      scheduleDiv.innerHTML = ""; // Clear existing schedule
+      const errorMessageDiv = document.getElementById("error-message");
+
+      if (appointments.length === 0) {
+        scheduleDiv.style.display = "none";
+        errorMessageDiv.style.display = "block";
+      } else {
+        scheduleDiv.style.display = "block";
+        errorMessageDiv.style.display = "none";
+      }
+
+      // Filter out cancelled lessons if there are multiple lessons for the same hour
+      const filteredAppointments = filterCancelledLessons(appointments);
+
+      filteredAppointments.forEach((appointment) => {
+        const startTime = new Date(appointment.start * 1000);
+        const endTime = new Date(appointment.end * 1000);
+
+        // Format start and end times
+        const startTimeString = startTime.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+        const endTimeString = endTime.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+
+        // Object met vak afkortingen en hun volledige namen
+        const subjectsMapping = {
+          ak: "Aardrijkskunde",
+          en: "Engels",
+          fa: "Frans",
+          gd: "Godsdienst",
+          gs: "Geschiedenis",
+          ha: "Handvaardigheid",
+          ict: "Informatiekunde",
+          in: "Informatiekunde",
+          kcv: "Klassieke Culturele Vorming",
+          lo: "Lichamelijke opvoeding",
+          men: "Mentorles",
+          mn: "Mens en natuur",
+          mu: "Muziek",
+          ne: "Nederlands",
+          te: "Tekenen/Techniek",
+          wi: "Wiskunde",
+          fr: "Frans",
+          nl: "Nederlands",
+          du: "Duits",
+          bi: "Biologie",
+          na: "Natuurkunde",
+          sk: "Scheikunde",
+          nask: "Natuurkunde/Scheikunde",
+          ec: "Economie",
+          econ: "Economie",
+          ma: "Maatschappijleer",
+          be: "Beeldende Vorming",
+          kv: "Kunstvakken",
+          fi: "Filosofie",
+          la: "Latijn",
+          gr: "Grieks",
+          PROJECT: "Project",
+          rkn: "Rekentoets",
+          pe: "Physical education",
+          ontw: "Ontwerpen",
+          ltc: "Latijn",
+          gtc: "Grieks",
+          entl: "Engels",
+          ges: "Geschiedenis",
+          fatl: "Frans",
+          biol: "Biologie",
+          netl: "Nederlands",
+          gds: "Godsdienst",
+          wis: "Wiskunde",
+          die: "Diëtetiek",
+          kubv: "Kunst Beeldende Vakken",
+          bg: "Begeleiding",
+          sp: "Spaans",
+          BSA: "Bindend studieadvies",
+          bsa: "Bindend studieadvies",
+        };
+
+        // Map subjects abbreviations to full names
+        const subjectsFullNames = appointment.subjects.map(
+          (subject) => subjectsMapping[subject] || subject
+        );
+
+        let changeDescription = "";
+        if (appointment.changeDescription) {
+          changeDescription = `<p>${appointment.changeDescription}</p>`;
         }
 
-        // Filter out cancelled lessons if there are multiple lessons for the same hour
-        const filteredAppointments = filterCancelledLessons(appointments);
-
-        filteredAppointments.forEach((appointment) => {
-          const startTime = new Date(appointment.start * 1000);
-          const endTime = new Date(appointment.end * 1000);
-
-          // Format start and end times
-          const startTimeString = startTime.toLocaleTimeString([], {
-            hour: "2-digit",
-            minute: "2-digit",
-          });
-          const endTimeString = endTime.toLocaleTimeString([], {
-            hour: "2-digit",
-            minute: "2-digit",
-          });
-
-          // Object met vak afkortingen en hun volledige namen
-          const subjectsMapping = {
-            ak: "Aardrijkskunde",
-            en: "Engels",
-            fa: "Frans",
-            gd: "Godsdienst",
-            gs: "Geschiedenis",
-            ha: "Handvaardigheid",
-            ict: "Informatiekunde",
-            in: "Informatiekunde",
-            kcv: "Klassieke Culturele Vorming",
-            lo: "Lichamelijke opvoeding",
-            men: "Mentorles",
-            mn: "Mens en natuur",
-            mu: "Muziek",
-            ne: "Nederlands",
-            te: "Tekenen/Techniek",
-            wi: "Wiskunde",
-            fr: "Frans",
-            nl: "Nederlands",
-            du: "Duits",
-            bi: "Biologie",
-            na: "Natuurkunde",
-            sk: "Scheikunde",
-            nask: "Natuurkunde/Scheikunde",
-            ec: "Economie",
-            econ: "Economie",
-            ma: "Maatschappijleer",
-            be: "Beeldende Vorming",
-            kv: "Kunstvakken",
-            fi: "Filosofie",
-            la: "Latijn",
-            gr: "Grieks",
-            PROJECT: "Project",
-            rkn: "Rekentoets",
-            pe: "Physical education",
-            ontw: "Ontwerpen",
-            ltc: "Latijn",
-            gtc: "Grieks",
-            entl: "Engels",
-            ges: "Geschiedenis",
-            fatl: "Frans",
-            biol: "Biologie",
-            netl: "Nederlands",
-            gds: "Godsdienst",
-            wis: "Wiskunde",
-            die: "Diëtetiek",
-            kubv: "Kunst Beeldende Vakken",
-            bg: "Begeleiding",
-            sp: "Spaans",
-            BSA: "Bindend studieadvies",
-            bsa: "Bindend studieadvies",
-          };
-
-          // Map subjects abbreviations to full names
-          const subjectsFullNames = appointment.subjects.map(
-            (subject) => subjectsMapping[subject] || subject
-          );
-
-          let changeDescription = "";
-          if (appointment.changeDescription) {
-            changeDescription = `<p>${appointment.changeDescription}</p>`;
-          }
-
-          // Create appointment HTML
-          const appointmentDiv = document.createElement("div");
-          appointmentDiv.innerHTML = `
-            <p><strong id="vaknaam">${subjectsFullNames.join(
-              ", "
-            )}</strong><strong style="position:absolute;right:25px;">${
-            appointment.startTimeSlot
-          }</strong></p>
-            <p>${startTimeString} - ${endTimeString} <span style="margin-left: 10px;">${appointment.locations.join(
+        // Create appointment HTML
+        const appointmentDiv = document.createElement("div");
+        appointmentDiv.innerHTML = `
+          <p><strong id="vaknaam">${subjectsFullNames.join(
             ", "
-          )} (${appointment.teachers.join(", ")})</span></p>
-          <p class="className">${appointment.groups.join(", ")}</p>
-          <p>
-            ${
-              appointment.changeDescription
-                ? '<svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="vertical-align: sub;"><path d="M10.909 2.782a2.25 2.25 0 0 1 2.975.74l.083.138 7.759 14.009a2.25 2.25 0 0 1-1.814 3.334l-.154.006H4.242A2.25 2.25 0 0 1 2.2 17.812l.072-.143L10.03 3.66a2.25 2.25 0 0 1 .879-.878ZM12 16.002a.999.999 0 1 0 0 1.997.999.999 0 0 0 0-1.997Zm-.002-8.004a1 1 0 0 0-.993.884L11 8.998 11 14l.007.117a1 1 0 0 0 1.987 0l.006-.117L13 8.998l-.007-.117a1 1 0 0 0-.994-.883Z" fill="#ff9800"/></svg>'
-                : ""
-            }
-            ${appointment.changeDescription}
-          </p>
-          `;
-
-          // Add cancelled class if appointment is cancelled
-          if (appointment.cancelled === true) {
-            appointmentDiv.classList.add("cancelled");
+          )}</strong><strong style="position:absolute;right:25px;">${
+          appointment.startTimeSlot
+        }</strong></p>
+          <p>${startTimeString} - ${endTimeString} <span style="margin-left: 10px;">${appointment.locations.join(
+          ", "
+        )} (${appointment.teachers.join(", ")})</span></p>
+        <p class="className">${appointment.groups.join(", ")}</p>
+        <p>
+          ${
+            appointment.changeDescription
+              ? '<svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="vertical-align: sub;"><path d="M10.909 2.782a2.25 2.25 0 0 1 2.975.74l.083.138 7.759 14.009a2.25 2.25 0 0 1-1.814 3.334l-.154.006H4.242A2.25 2.25 0 0 1 2.2 17.812l.072-.143L10.03 3.66a2.25 2.25 0 0 1 .879-.878ZM12 16.002a.999.999 0 1 0 0 1.997.999.999 0 0 0 0-1.997Zm-.002-8.004a1 1 0 0 0-.993.884L11 8.998 11 14l.007.117a1 1 0 0 0 1.987 0l.006-.117L13 8.998l-.007-.117a1 1 0 0 0-.994-.883Z" fill="#ff9800"/></svg>'
+              : ""
           }
+          ${appointment.changeDescription}
+        </p>
+        `;
 
-          scheduleDiv.appendChild(appointmentDiv);
-        });
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  });
+        // Add cancelled class if appointment is cancelled
+        if (appointment.cancelled === true) {
+          appointmentDiv.classList.add("cancelled");
+        }
+
+        scheduleDiv.appendChild(appointmentDiv);
+      });
+    })
+    .catch((error) => console.error("Error fetching data:", error));
 }
 
 // Function to filter out cancelled lessons if there are multiple lessons for the same hour
@@ -316,6 +321,7 @@ schoolName.oninput = () => {
 authorizationCode.value = localStorage.getItem("authorizationCode");
 authorizationCode.oninput = () => {
   localStorage.setItem("authorizationCode", authorizationCode.value);
+  localStorage.setItem("access_token", "undefined");
 };
 
 user.value = localStorage.getItem("user");
