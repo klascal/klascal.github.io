57b989147a765df6269b846cf8bbb326666ca211
FR1234 <74495215+blissradio@users.noreply.github.com>
2024-11-15 20:53:41 +0100

    de eventListener veranderd naar 'change'

--- scheduleDaytest.js
+++ scheduleDaytest.js
@@ -379,7 +379,7 @@ function filterCancelledLessons(appointments) {
 }
 
 // Function to handle loading schedule when button is clicked
-document.getElementById("dateInput").addEventListener("input", function () {
+document.getElementById("dateInput").addEventListener("change", function () {
   const dateInput = document.getElementById("dateInput").value;
   fetchAppointments(dateInput);
 });
