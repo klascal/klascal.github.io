b09559465c32ec8b459e7b948ffec1381c97ec63
klascal <158731903+klascal@users.noreply.github.com>
2025-02-09 19:22:57 +0100

    Swipen tussen dagen vloeiender gemaakt door will-change te gebruiken

--- scheduleDaytest.js
+++ scheduleDaytest.js
@@ -438,13 +438,13 @@ function fetchAppointments(date, focus) {
         if (appointment.appointmentInstance == null) {
           warning = "Afgemeld";
           warningsymbol = warning
-            ? `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" id="icon" style="margin-right: 1px"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q54 0 104-17.5t92-50.5L228-676q-33 42-50.5 92T160-480q0 134 93 227t227 93Zm252-124q33-42 50.5-92T800-480q0-134-93-227t-227-93q-54 0-104 17.5T284-732l448 448Z"/></svg>`
+            ? `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" id="icon" style="margin-right: 2.5px"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q54 0 104-17.5t92-50.5L228-676q-33 42-50.5 92T160-480q0 134 93 227t227 93Zm252-124q33-42 50.5-92T800-480q0-134-93-227t-227-93q-54 0-104 17.5T284-732l448 448Z"/></svg>`
             : "";
         }
         if (appointment.schedulerRemark !== "") {
           warning = appointment.schedulerRemark;
           warningsymbol = warning
-            ? `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" id="icon" style="margin-right: 1px"><path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>`
+            ? `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" id="icon" style="margin-right: 2.5px"><path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>`
             : "";
         }
         if (
@@ -464,7 +464,7 @@ function fetchAppointments(date, focus) {
         appointmentDiv.innerHTML = `
           <p><strong id="vaknaam">${subjectsFullNames.join(
             ", "
-          )}</strong><strong style="position:absolute;right:7.5px;" id="timeSlot">${info}${timeSlot}</strong></p>
+          )}</strong><strong style="float: right; margin-right: 9px" id="timeSlot">${info}${timeSlot}</strong></p>
           <p>${startTimeString} - ${endTimeString} <span style="margin-left: 10px;">${appointment.locations.join(
           ", "
         )} ${teachers == "()" ? "" : teachers} <span class="warning">
@@ -915,16 +915,16 @@ document.getElementById("previousDay").addEventListener("click", function () {
   document.getElementById("dateInput").value = nextDay;
   if (currentDay == 1) {
     document.getElementById("schedule").style =
-      "animation: day1left 0.15s ease-in-out forwards; right: 0;";
+      "animation: day1left 0.1s ease-in-out forwards; right: 0;";
   } else if (currentDay == 2) {
     document.getElementById("schedule").style =
-      "animation: day2left 0.15s ease-in-out forwards; right: 100vw;";
+      "animation: day2left 0.1s ease-in-out forwards; right: 100vw;";
   } else if (currentDay == 3) {
     document.getElementById("schedule").style =
-      "animation: day3left 0.15s ease-in-out forwards; right: 200vw;";
+      "animation: day3left 0.1s ease-in-out forwards; right: 200vw;";
   } else if (currentDay == 4) {
     document.getElementById("schedule").style =
-      "animation: day4left 0.15s ease-in-out forwards; right: 300vw;";
+      "animation: day4left 0.1s ease-in-out forwards; right: 300vw;";
   } else if (currentDay == 5) {
     document.getElementById("schedule").style = "right: 400vw;";
   } else if (currentDay == 6) {
@@ -999,16 +999,16 @@ document.getElementById("nextDay").addEventListener("click", function () {
     document.getElementById("schedule").style = "right: 0;";
   } else if (currentDay == 2) {
     document.getElementById("schedule").style =
-      "animation: day2 0.15s ease-in-out forwards; right: 100vw;";
+      "animation: day2 0.1s ease-in-out forwards; right: 100vw;";
   } else if (currentDay == 3) {
     document.getElementById("schedule").style =
-      "animation: day3 0.15s ease-in-out forwards; right: 200vw;";
+      "animation: day3 0.1s ease-in-out forwards; right: 200vw;";
   } else if (currentDay == 4) {
     document.getElementById("schedule").style =
-      "animation: day4 0.15s ease-in-out forwards; right: 300vw;";
+      "animation: day4 0.1s ease-in-out forwards; right: 300vw;";
   } else if (currentDay == 5) {
     document.getElementById("schedule").style =
-      "animation: day5 0.15s ease-in-out forwards; right: 400vw;";
+      "animation: day5 0.1s ease-in-out forwards; right: 400vw;";
   } else if (currentDay == 6) {
     document.getElementById("schedule").style = "right: 500vw;";
   } else if (currentDay == 0) {
