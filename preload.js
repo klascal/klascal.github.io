c611267c996b60afea8542ff84f378a56a36c958
klascal <158731903+klascal@users.noreply.github.com>
2025-02-14 21:26:23 +0100

    Oude bestanden verwijderd

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron/renderer");
-
-contextBridge.exposeInMainWorld("shell", {
-  open: () => ipcRenderer.send("shell:open"),
-});
--- renderer.js
+++
@@ -1,8 +0,0 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All APIs exposed by the context bridge are available here.
-
-// Binds the buttons to the context bridge API.
-document.getElementById("open-in-browser").addEventListener("click", () => {
-  window.shell.open();
-});
