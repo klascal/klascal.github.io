cb547f969635618557ef79c3a38862ec9aaf145f
klascal <158731903+klascal@users.noreply.github.com>
2025-04-14 12:01:37 +0200

    Elke 5m rooster ophalen gefikst. Afgemelde lessen correct gestyled.

--- schedule.js
+++ schedule.js
@@ -6,14 +6,7 @@ dialogs.forEach((dialog) => {
 });
 // Haal elke 5 minuten het rooster op
 setInterval(function () {
-  if (
-    document.getElementById("dateInput").value !== "" &&
-    localStorage.getItem("access_token") &&
-    localStorage.getItem("schoolName") &&
-    localStorage.getItem("userType")
-  ) {
-    handleFormSubmit();
-  }
+  handleFormSubmit();
 }, 300000);
 sessionStorage.setItem("transform", "");
 const startTime = document.getElementById("startTime");
@@ -517,6 +510,7 @@ function displaySchedule(scheduleData) {
             warningsymbol = warning
               ? `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" id="icon"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q54 0 104-17.5t92-50.5L228-676q-33 42-50.5 92T160-480q0 134 93 227t227 93Zm252-124q33-42 50.5-92T800-480q0-134-93-227t-227-93q-54 0-104 17.5T284-732l448 448Z"/></svg>`
               : "";
+            ltr += " notEnrolled";
           } else if (appointment.schedulerRemark !== "") {
             warning = appointment.schedulerRemark;
             warningsymbol = warning
