890cdddcced2ba8efeccbc129d2a09a38239d3b1
BAJY-KO <a.j.baas@o2g2.nl>
2025-08-27 14:21:00 +0200

    Prefix van klas invoerveld
    Invoerveld toegevoegd voor het opgeven van een prefix dat elke klas heeft, zodat dat in het rooster weggelaten wordt.

--- schedule.js
+++ schedule.js
@@ -806,9 +806,15 @@ function displaySchedule(scheduleData) {
               ? `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" id="icon"><path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>`
               : "";
           }
+		  const groups = appointment.groups;
+		  groups.forEach((group, index) => {
+			if (group.startsWith(localStorage.getItem("prefix-group"))) {
+			  groups[index] = group.slice(localStorage.getItem("prefix-group").length); //groups[index] instead of group is necessary to actually change the value of group
+			}
+		  });
           const top =
-            localStorage.getItem("userType") === "teacher" && appointment.groups.length > 0
-              ? appointment.groups.join(", ")
+            localStorage.getItem("userType") === "teacher" && groups.length > 0
+              ? groups.join(", ")
               : subjects.join(", ");
           // Generate HTML for each
           // margin-left en width voor ltr
@@ -837,7 +843,7 @@ function displaySchedule(scheduleData) {
                 </div>
               </span>
               <p class="className">
-                ${appointment.groups.join(", ")}
+                ${groups.join(", ")}
               </p>
               <p class="subjectName">
                 ${subjects.join(",")}
@@ -1858,6 +1864,11 @@ css.oninput = () => {
 if (localStorage.getItem("css")) {
   document.getElementById("styling").innerText = localStorage.getItem("css");
 }
+const prefix_group = document.getElementById("prefix-group");
+prefix_group.value = localStorage.getItem("prefix-group");
+prefix_group.oninput = () => {
+  localStorage.setItem("prefix-group", prefix_group.value);
+};
 document.getElementById("weekBtn").addEventListener("click", () => {
   localStorage.setItem("dag", "false");
   if (!document.startViewTransition) {
