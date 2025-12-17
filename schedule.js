ae8d01548d6b00ca71b5b33543a7f9cbeb1f3a60
klascal <158731903+klascal@users.noreply.github.com>
2025-12-17 17:55:55 +0100

    Knoppen in de navigatiebalk aangepast. Google Sans Flex wordt nu ook gebruikt voor de knoppen i.p.v. Inter. Alternatieve lettertypes toegevoegd voor als het nog niet geladen is.

--- schedule.js
+++ schedule.js
@@ -261,7 +261,7 @@ function show(id, title, hideBack) {
     if (id !== "submenus" && !hideBack) {
       document.querySelector("#dialog h2").removeAttribute("data-translate");
       document.querySelector("#dialog h2").innerHTML =
-        '<button style="all: unset" onclick="show(`submenus`, `Instellingen`)"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" id="icon"><path d="m313-440 196 196q12 12 11.5 28T508-188q-12 11-28 11.5T452-188L188-452q-6-6-8.5-13t-2.5-15q0-8 2.5-15t8.5-13l264-264q11-11 27.5-11t28.5 11q12 12 12 28.5T508-715L313-520h447q17 0 28.5 11.5T800-480q0 17-11.5 28.5T760-440H313Z"/></svg></button>' +
+        '<button style="all: unset" onclick="show(`submenus`, `Instellingen`)"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" id="icon" style="padding: 0"><path d="m313-440 196 196q12 12 11.5 28T508-188q-12 11-28 11.5T452-188L188-452q-6-6-8.5-13t-2.5-15q0-8 2.5-15t8.5-13l264-264q11-11 27.5-11t28.5 11q12 12 12 28.5T508-715L313-520h447q17 0 28.5 11.5T800-480q0 17-11.5 28.5T760-440H313Z"/></svg></button>' +
         title;
     } else {
       document.querySelector("#dialog h2").innerHTML = title;
