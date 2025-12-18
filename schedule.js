11fa6c67f352f02895a11bbb4c5c06d64a8a141d
klascal <158731903+klascal@users.noreply.github.com>
2025-12-18 21:15:21 +0100

    Iconen in instellingen weer aangepast. Schakelen tussen jaren werkt weer. Weken onder de 10 werken nu. Media queries verder gefinetuned.

--- schedule.js
+++ schedule.js
@@ -261,7 +261,7 @@ function show(id, title, hideBack) {
     if (id !== "submenus" && !hideBack) {
       document.querySelector("#dialog h2").removeAttribute("data-translate");
       document.querySelector("#dialog h2").innerHTML =
-        '<button style="all: unset" onclick="show(`submenus`, `Instellingen`)"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" id="icon" style="padding: 0"><path d="m313-440 196 196q12 12 11.5 28T508-188q-12 11-28 11.5T452-188L188-452q-6-6-8.5-13t-2.5-15q0-8 2.5-15t8.5-13l264-264q11-11 27.5-11t28.5 11q12 12 12 28.5T508-715L313-520h447q17 0 28.5 11.5T800-480q0 17-11.5 28.5T760-440H313Z"/></svg></button>' +
+        '<button style="all: unset" onclick="show(`submenus`, `Instellingen`)"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" id="icon" style="padding: 0; transform: translateY(-2px); margin-right: 5.55px;"><path d="m313-440 196 196q12 12 11.5 28T508-188q-12 11-28 11.5T452-188L188-452q-6-6-8.5-13t-2.5-15q0-8 2.5-15t8.5-13l264-264q11-11 27.5-11t28.5 11q12 12 12 28.5T508-715L313-520h447q17 0 28.5 11.5T800-480q0 17-11.5 28.5T760-440H313Z"/></svg></button>' +
         title;
     } else {
       document.querySelector("#dialog h2").innerHTML = title;
@@ -305,6 +305,7 @@ async function fetchSchedule(year, week, isFirstLoad) {
   if (!week) week = new Date().getWeek();
   window.week = week;
   window.year = year;
+  if (week < 10) week = `0${week}`; // Voeg een voorloopnul toe aan enkelcijferige weken
   document.getElementById(
     "week"
   ).innerHTML = `<p style="color: var(--accent-text-faded)">Wk</p><p style="font-weight: 600;font-size: 1.125rem;">${week}</p>`;
@@ -691,6 +692,13 @@ function switchDay(dir) {
     day--;
     week--;
   }
+  if (week > 52) {
+    week = 1;
+    window.year++;
+  } else if (week == 0) {
+    week = 52;
+    window.year--;
+  }
   if (day == -1) {
     day = 4;
     behavior = "instant";
@@ -724,7 +732,7 @@ window.addEventListener("resize", () => {
     left: window.innerWidth * day,
     behavior: "instant",
   });
-  if (window.innerWidth < 330) {
+  if (window.innerWidth < 346) {
     document.getElementById("dayBtn").click();
   }
 });
